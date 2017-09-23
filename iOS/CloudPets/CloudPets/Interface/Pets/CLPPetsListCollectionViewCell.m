eb25180bbdb62193d0db4fb8c5e40f856516268d
icestraw <icestarwms@gmail.com>
2017-09-23 19:42:41 +0800

    Home

---
+++ iOS/CloudPets/CloudPets/Interface/Pets/CLPPetsListCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  CLPPetsListCollectionViewCell.m
+//  CloudPets
+//
+//  Created by ice on 23/09/2017.
+//  Copyright © 2017 icestraw. All rights reserved.
+//
+
+#import "CLPPetsListCollectionViewCell.h"
+
+@implementation CLPPetsListCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
